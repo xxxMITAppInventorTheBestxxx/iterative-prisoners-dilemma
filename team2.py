eed12eba95ebe20782de18ef15d7761f0ea4e08a
xxxMITAppInventorTheBestxxx <34242093+xxxMITAppInventorTheBestxxx@users.noreply.github.com>
2017-12-13 08:46:05 -0600

    ddd
    ddd

--- team2.py
+++ team2.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'TigerTony' # Only 10 chars displayed.
+strategy_name = 'Cooperate then Copy'
+strategy_description = 'Always cooperate first then do what your opponent does'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
